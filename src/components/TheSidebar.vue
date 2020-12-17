4e9468b4bdfce6e5cc13b2b8eca4c2e7a31e0cc3
Joshua Abad <yeyoan@gmail.com>
2020-12-17 15:29:20 +0800

    Add icons to nav links

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -3,17 +3,21 @@
     class="transform overflow-x-hidden top-0 left-0 w-64 bg-white dark:bg-gray-800 fixed h-full overflow-auto ease-in-out transition-all duration-300 border-r border-gray-200 dark:border-gray-700 z-10"
     :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
   >
-    <div class="h-20">
-    </div>
+    <div class="h-20"></div>
     <div class="flex flex-col">
       <router-link
         :key="i"
-        v-for="(content, i) in contents"
         :to="content.url"
         @click="$emit('nav-click', $event, content.url)"
-        class="block text-sm hover:bg-opacity-25 dark:text-gray-100 dark:hover:bg-gray-700 px-5 py-4 hover:bg-gray-200 active:bg-blue-400 border-b border-gray-300 dark:border-gray-700"
+        v-for="(content, i) in contents"
+        class="flex items-center w-full px-5 py-4 hover:bg-opacity-25 border-b border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        {{ content.label }}
+        <div class="fill-current w-4 h-4 dark:text-gray-500" v-html="content.icon" />
+        <div
+          class="ml-4 dark:text-gray-100"
+        >
+          {{ content.label }}
+        </div>
       </router-link>
     </div>
   </aside>
@@ -33,23 +37,27 @@ export default defineComponent({
       contents: [
         {
           label: 'Home',
-          url: '/'
+          url: '/',
+          icon:
+            '<svg fill="currentColor" viewBox="0 0 20 20"><defs/><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/></svg>'
         },
         {
           label: 'Courses',
-          url: '/courses'
+          url: '/courses',
+          icon:
+            '<svg fill="currentColor" viewBox="0 0 20 20"><defs/><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"/></svg>'
         },
         {
           label: 'Calendar',
-          url: '/'
+          url: '/',
+          icon:
+            '<svg fill="currentColor" viewBox="0 0 20 20"><defs/><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/></svg>'
         },
         {
           label: 'Private Files',
-          url: '/'
-        },
-        {
-          label: 'My courses',
-          url: '/'
+          url: '/',
+          icon:
+            '<svg fill="currentColor" viewBox="0 0 20 20"><defs/><path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd"/></svg>'
         }
       ]
     }
