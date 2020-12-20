fd7ff906cc281d879a116bdf29cdb4771c56107b
Joshua Abad <yeyoan@gmail.com>
2020-12-20 19:02:52 +0800

    Use toggle buttons to change theme

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <label :for="label" class="mt-3 inline-flex items-center cursor-pointer">
+    <span class="relative">
+      <span
+        class="block w-10 h-6 rounded-full shadow-inner"
+        :class="modelValue ? 'bg-green-500' : 'bg-gray-400'"
+      ></span>
+      <span
+        class="absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow-lg inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out"
+        :class="modelValue ? 'transform translate-x-full' : ''"
+      >
+        <input
+          :value="modelValue"
+          @change="$emit('update:modelValue', $event.target.checked)"
+          :id="label"
+          type="checkbox"
+          class="absolute opacity-0 w-0 h-0"
+        />
+      </span>
+    </span>
+    <span class="ml-3 text-sm">{{ label }}</span>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ToggleButton',
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    label: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  emits: ['update:modelValue']
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,27 @@
   <div class="home dark:text-gray-200 text-xl">
     <div v-if="user">
       Home page
-      <h1>Appearance</h1>
-      <div class="inline-flex flex-col space-y-5">
-        <BaseButton
-          @click="handleChangeTheme('light')"
-          label="Light"
-          :filled="!(theme === 'light')"
-          class="mt-5"
-        />
-        <BaseButton
-          @click="handleChangeTheme('dark')"
-          label="Dark Theme"
-          :filled="!(theme === 'dark')"
-        />
-        <BaseButton
-          @click="handleChangeTheme(null)"
-          label="System"
-          :filled="!(theme === null)"
-        />
+      <div class="mt-4">
+        <h1 class="uppercase font-semibold tracking-wide text-sm">
+          Appearance
+        </h1>
+        <div>
+          <ToggleButton
+            v-model="automatic"
+            label="Automatic (follows system settings)"
+          />
+        </div>
+        <div>
+          <ToggleButton
+            v-model="darkMode"
+            label="Dark Mode"
+            v-show="!automatic"
+          />
+        </div>
       </div>
-      <RecentCourses />
+      <div class="mt-4">
+        <RecentCourses />
+      </div>
     </div>
     <div v-else>you are not logged in smh</div>
   </div>
@@ -30,13 +31,38 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Theme, User } from '@/types'
-import BaseButton from '@/components/BaseButton.vue'
 import RecentCourses from '@/components/RecentCourses.vue'
 import { SET_THEME } from '@/store/mutation-types'
+import ToggleButton from '@/components/ToggleButton.vue'
 
 export default defineComponent({
-  components: { BaseButton, RecentCourses },
+  components: { RecentCourses, ToggleButton },
   name: 'Home',
+  data () {
+    return {
+      automatic: false,
+      darkMode: false
+    }
+  },
+  mounted () {
+    if (this.theme) {
+      this.darkMode = this.theme === 'dark'
+    } else {
+      this.automatic = true
+    }
+  },
+  watch: {
+    automatic (enabled: boolean) {
+      if (enabled) {
+        this.handleChangeTheme(null)
+      } else {
+        this.handleChangeTheme(this.darkMode ? 'dark' : 'light')
+      }
+    },
+    darkMode (enabled: boolean) {
+      this.handleChangeTheme(enabled ? 'dark' : 'light')
+    }
+  },
   methods: {
     handleChangeTheme (theme: Theme) {
       this.$store.commit(SET_THEME, theme)
