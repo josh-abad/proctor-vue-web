88d36a1d7570fcee738da8b39c69feda7d2d4e31
Joshua Abad <yeyoan@gmail.com>
2020-12-21 21:43:31 +0800

    Change how CoursesPage is displayed according to role

--- src/views/CoursesPage.vue
+++ src/views/CoursesPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="bg-white dark:bg-gray-800 shadow rounded-xl px-6 py-6">
-      <h1 class="text-lg font-semibold">Course overview</h1>
+      <h1 class="text-lg font-semibold">{{ header}}</h1>
       <div class="grid grid-cols-3 gap-6 mt-3 sm:grid-cols-2 md:grid-cols-3">
         <div :key="course.id" v-for="course in courses">
           <CourseCard :course="course" />
@@ -13,7 +13,7 @@
 
 <script lang="ts">
 import CourseCard from '@/components/CourseCard.vue'
-import { Course } from '@/types'
+import { Course, Role } from '@/types'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -21,7 +21,19 @@ export default defineComponent({
   name: 'CoursesPage',
   computed: {
     courses (): Course[] {
+      if (this.$store.getters.userRole === 'admin') {
+        return this.$store.state.courses
+      }
       return this.$store.getters.getUserCourses
+    },
+    header (): string {
+      const userRole: Role = this.$store.getters.userRole
+      if (userRole === 'student') {
+        return 'Course overview'
+      } else if (userRole === 'coordinator') {
+        return 'Courses you are coordinator of'
+      }
+      return 'Martin Coursese'
     }
   }
 })
