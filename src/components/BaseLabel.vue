5c9d23ce8963c15a7cc6ff9825ac84c61c1fea06
Joshua Abad <yeyoan@gmail.com>
2020-12-26 21:35:50 +0800

    Decrease padding

--- src/components/BaseLabel.vue
+++ src/components/BaseLabel.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="uppercase text-xs text-gray-600 dark:text-gray-400 font-semibold tracking-wide mb-2"
+    class="uppercase text-xs text-gray-600 dark:text-gray-400 font-semibold tracking-wide mb-1"
     :class="line ? 'apply pb-1 border-b dark:border-gray-700' : ''"
   >
     <slot></slot>
