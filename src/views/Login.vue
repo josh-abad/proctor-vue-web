3288b34693ce909e020847ae25808c70089aaeed
Joshua Abad <yeyoan@gmail.com>
2020-12-26 22:28:18 +0800

    Add logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,22 @@
     <div class="flex items-center justify-center">
       <Center />
       <BasePanel class="px-10 py-8">
-        <form>
-          <h3 class="font-semibold text-lg">Login</h3>
+        <div class="flex justify-center">
+          <img
+            :src="require(`@/assets/${logoFilename}`)"
+            alt="Logo"
+            class="h-7"
+          />
+        </div>
+        <form class="mt-8">
+          <h3 class="font-medium">Login</h3>
           <p class="text-xs dark:text-gray-400">
             By continuing, you agree to our
-            <router-link to="#" class="text-green-400"
+            <router-link to="#" class="text-green-500"
               >User Agreement</router-link
             >
             and
-            <router-link to="#" class="text-green-400"
+            <router-link to="#" class="text-green-500"
               >Privacy Policy</router-link
             >.
           </p>
@@ -48,14 +55,14 @@
           </div>
           <p class="mt-3 text-xs">
             Forgot your
-            <router-link to="#" class="text-green-400">username</router-link> or
-            <router-link to="#" class="text-green-400">password</router-link>?
+            <router-link to="#" class="text-green-500">username</router-link> or
+            <router-link to="#" class="text-green-500">password</router-link>?
           </p>
           <p class="mt-3 text-xs">
             New to Proctor Vue?
             <router-link
               to="/register"
-              class="uppercase font-semibold tracking-wide text-green-400"
+              class="uppercase font-semibold tracking-wide text-green-500"
               >Sign Up</router-link
             >
           </p>
@@ -73,6 +80,7 @@ import BaseLabel from '@/components/BaseLabel.vue'
 import BasePanel from '@/components/BasePanel.vue'
 import Center from '@/components/Center.vue'
 import { LOG_IN } from '@/store/action-types'
+import { Theme } from '@/types'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -87,6 +95,14 @@ export default defineComponent({
   computed: {
     fieldsFilled (): boolean {
       return this.username.length > 0 && this.password.length > 0
+    },
+    logoFilename (): string {
+      const theme: Theme = this.$store.state.theme
+
+      if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        return 'logo-white.png'
+      }
+      return 'logo.png'
     }
   },
   methods: {
--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -1,83 +1,90 @@
 <template>
-<div class="fixed inset-0">
-  <div class="flex justify-center items-center">
-    <Center />
-    <BasePanel class="px-10 py-8">
-      <form>
-        <h3 class="font-semibold text-lg">Sign up</h3>
-        <p class="text-xs dark:text-gray-400">
-          By continuing, you agree to our
-          <router-link to="#" class="text-green-400"
-            >User Agreement</router-link
-          >
-          and
-          <router-link to="#" class="text-green-400">Privacy Policy</router-link
-          >.
-        </p>
-        <div class="flex">
-          <div class="mt-8 mr-4">
-            <label for="firstName">
-              <BaseLabel>First Name</BaseLabel>
+  <div class="fixed inset-0">
+    <div class="flex justify-center items-center">
+      <Center />
+      <BasePanel class="px-10 py-8">
+        <div class="flex justify-center">
+          <img
+            :src="require(`@/assets/${logoFilename}`)"
+            alt="Logo"
+            class="h-7"
+          />
+        </div>
+        <form class="mt-8">
+          <h3 class="font-semibold text-lg">Sign up</h3>
+          <p class="text-xs dark:text-gray-400">
+            By continuing, you agree to our
+            <router-link to="#" class="text-green-500"
+              >User Agreement</router-link
+            >
+            and
+            <router-link to="#" class="text-green-500">Privacy Policy</router-link
+            >.
+          </p>
+          <div class="flex">
+            <div class="mt-8 mr-4">
+              <label for="firstName">
+                <BaseLabel>First Name</BaseLabel>
+              </label>
+              <BaseInput id="firstName" v-model="firstName" type="text" />
+            </div>
+            <div class="mt-8">
+              <label for="lastName">
+                <BaseLabel>Last Name</BaseLabel>
+              </label>
+              <BaseInput id="lastName" v-model="lastName" type="text" />
+            </div>
+          </div>
+          <div class="mt-4">
+            <label for="username">
+              <BaseLabel>Username</BaseLabel>
             </label>
-            <BaseInput id="firstName" v-model="firstName" type="text" />
+            <BaseInput id="username" class="w-full" v-model="username" type="text" />
+          </div>
+          <div class="mt-4">
+            <label for="password">
+              <BaseLabel>Password</BaseLabel>
+            </label>
+            <BaseInput
+              id="password"
+              class="w-full"
+              v-model="password"
+              type="password"
+            />
+          </div>
+          <div class="mt-4">
+            <label for="confirmPassword">
+              <BaseLabel>Confirm Password</BaseLabel>
+            </label>
+            <BaseInput
+              id="confirmPassword"
+              class="w-full"
+              v-model="confirmPassword"
+              type="password"
+            />
           </div>
           <div class="mt-8">
-            <label for="lastName">
-              <BaseLabel>Last Name</BaseLabel>
-            </label>
-            <BaseInput id="lastName" v-model="lastName" type="text" />
+            <BaseButton
+              @click.prevent="handleRegister"
+              :disabled="!passwordsEqual"
+              type="submit"
+              prominent
+            >
+              Sign Up
+            </BaseButton>
           </div>
-        </div>
-        <div class="mt-4">
-          <label for="username">
-            <BaseLabel>Username</BaseLabel>
-          </label>
-          <BaseInput id="username" class="w-full" v-model="username" type="text" />
-        </div>
-        <div class="mt-4">
-          <label for="password">
-            <BaseLabel>Password</BaseLabel>
-          </label>
-          <BaseInput
-            id="password"
-            class="w-full"
-            v-model="password"
-            type="password"
-          />
-        </div>
-        <div class="mt-4">
-          <label for="confirmPassword">
-            <BaseLabel>Confirm Password</BaseLabel>
-          </label>
-          <BaseInput
-            id="confirmPassword"
-            class="w-full"
-            v-model="confirmPassword"
-            type="password"
-          />
-        </div>
-        <div class="mt-8">
-          <BaseButton
-            @click.prevent="handleRegister"
-            :disabled="!passwordsEqual"
-            type="submit"
-            prominent
-          >
-            Sign Up
-          </BaseButton>
-        </div>
-        <p class="mt-3 text-xs">
-          Already have an account?
-          <router-link
-            to="/login"
-            class="uppercase font-semibold tracking-wide text-green-400"
-            >Log In</router-link
-          >
-        </p>
-      </form>
-    </BasePanel>
+          <p class="mt-3 text-xs">
+            Already have an account?
+            <router-link
+              to="/login"
+              class="uppercase font-semibold tracking-wide text-green-500"
+              >Log In</router-link
+            >
+          </p>
+        </form>
+      </BasePanel>
+    </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -87,7 +94,7 @@ import BaseLabel from '@/components/BaseLabel.vue'
 import BasePanel from '@/components/BasePanel.vue'
 import Center from '@/components/Center.vue'
 import { SIGN_UP } from '@/store/action-types'
-import { UserCredentials } from '@/types'
+import { Theme, UserCredentials } from '@/types'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -105,6 +112,14 @@ export default defineComponent({
   computed: {
     passwordsEqual (): boolean {
       return this.password.length > 0 && this.password === this.confirmPassword
+    },
+    logoFilename (): string {
+      const theme: Theme = this.$store.state.theme
+
+      if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        return 'logo-white.png'
+      }
+      return 'logo.png'
     }
   },
   methods: {
