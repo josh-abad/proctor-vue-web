fe3b506ef333090747c26764b0ef45938bd0d484
Joshua Abad <yeyoan@gmail.com>
2021-01-05 18:05:51 +0800

    Use compression plugin for webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const zlib = require('zlib')
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        filename: '[path][base].br',
+        algorithm: 'brotliCompress',
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+          }
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: false
+      })
+    ]
+  },
   devServer: {
     proxy: {
       '^/api': {
