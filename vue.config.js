55c8585f4988a5508720c552d5c8533ab35fcc56
Joshua Abad <yeyoan@gmail.com>
2021-01-05 21:06:20 +0800

    Use plugin in production only

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,30 @@ const CompressionPlugin = require('compression-webpack-plugin')
 const zlib = require('zlib')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        filename: '[path][base].br',
-        algorithm: 'brotliCompress',
-        test: /\.(js|css|html|svg)$/,
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11
-          }
-        },
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: false
-      })
-    ]
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins = [
+        new CompressionPlugin({
+          filename: '[path][base].gz',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+        new CompressionPlugin({
+          filename: '[path][base].br',
+          algorithm: 'brotliCompress',
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+            }
+          },
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      ]
+    }
   },
   devServer: {
     proxy: {
