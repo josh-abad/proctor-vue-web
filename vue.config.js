98b37122341b3b4679eb48af537e1080cfaf0f19
Joshua Abad <yeyoan@gmail.com>
2021-01-09 16:51:06 +0800

    Properly configure webpack plugins

--- vue.config.js
+++ vue.config.js
@@ -4,30 +4,28 @@ const zlib = require('zlib')
 module.exports = {
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
-      config = {
-        plugins: [
-          new CompressionPlugin({
-            filename: '[path][base].gz',
-            algorithm: 'gzip',
-            test: /\.js$|\.css$|\.html$/,
-            threshold: 10240,
-            minRatio: 0.8
-          }),
-          new CompressionPlugin({
-            filename: '[path][base].br',
-            algorithm: 'brotliCompress',
-            test: /\.(js|css|html|svg)$/,
-            compressionOptions: {
-              params: {
-                [zlib.constants.BROTLI_PARAM_QUALITY]: 11
-              }
-            },
-            threshold: 10240,
-            minRatio: 0.8
-          })
-        ]
-      }
-      return config
+      config.plugins = [
+        new CompressionPlugin({
+          filename: '[path][base].gz',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+        new CompressionPlugin({
+          filename: '[path][base].br',
+          algorithm: 'brotliCompress',
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+            }
+          },
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+        ...config.plugins
+      ]
     }
   },
   devServer: {
