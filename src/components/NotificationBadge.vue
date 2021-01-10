4f645b77d260d507a7a0785221ff1bfb4c349cf2
Joshua Abad <yeyoan@gmail.com>
2021-01-10 10:27:56 +0800

    Create badge component for notifications

---
+++ src/components/NotificationBadge.vue
@@ -0,0 +1,8 @@
+<template>
+  <span class="absolute -mr-1 -mt-1 top-0 right-0 flex w-3 h-3" v-once>
+    <span
+      class="animate-ping absolute inline-flex w-full h-full rounded-full bg-red-400 opacity-75"
+    />
+    <span class="relative inline-flex rounded-full w-3 h-3 bg-red-500" />
+  </span>
+</template>
