2d63c696a2b05c97af4f3e0066996605a96fe402
Joshua Abad <yeyoan@gmail.com>
2021-02-04 20:28:17 +0800

    Remove proxy rule

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:3001',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
-}
+module.exports = { }
