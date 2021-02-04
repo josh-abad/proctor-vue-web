8d134e1014fd2a5564f9e06f25de9957603ee1e2
Joshua Abad <yeyoan@gmail.com>
2021-02-04 20:24:00 +0800

    Remove /api from services\' urls

--- src/services/utils/config.ts
+++ src/services/utils/config.ts
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV === 'production' ? 'https://api.proctorvue.live' : ''
+export default process.env.NODE_ENV === 'production' ? 'https://api.proctorvue.live' : 'http://localhost:3001'
