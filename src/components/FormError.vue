90e44213009e74e0232de603a1bea5014d92bbe6
Joshua Abad <yeyoan@gmail.com>
2021-02-18 21:11:57 +0800

    Validate exam creation form

---
+++ src/components/FormError.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form-error form-error--border">
+    <!-- Heroicons name: exclamation -->
+    <svg class="form-error__icon" viewBox="0 0 20 20" fill="currentColor">
+      <path
+        fill-rule="evenodd"
+        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <div class="form-error__message"><slot /></div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.form-error {
+  @apply flex items-center bg-red-400 dark:bg-red-800 bg-opacity-20 dark:bg-opacity-10 px-2 py-1 rounded-md;
+}
+
+.form-error--border {
+  @apply border border-red-600 dark:border-red-300 border-opacity-20 dark:border-opacity-10;
+}
+
+.form-error__icon {
+  @apply w-4 h-4 fill-current mr-1;
+}
+
+.form-error__icon,
+.form-error__message {
+  @apply text-red-600 dark:text-red-300;
+}
+
+.form-error__message {
+  @apply text-sm;
+}
+</style>
--- src/views/courses/ExamCreationPage.vue
+++ src/views/courses/ExamCreationPage.vue
@@ -1,25 +1,26 @@
 <template>
-  <div v-if="course" class="p-4">
-    <BasePanel class="overflow-hidden">
-      <div
-        class="-mx-6 -mt-6 p-6 bg-gradient-to-r from-green-600 via-green-500 to-green-400"
-      >
-        <BaseLabel emphasis class="text-green-400">{{ course.name }}</BaseLabel>
-        <div
-          class="text-2xl font-bold text-white focus:outline-none"
-          contenteditable
-          @input="examName = $event.target.innerText"
-          v-text="examName"
-        ></div>
-      </div>
-      <div class="mt-4 flex divide-x divide-gray-700">
-        <div class="pr-6">
-          <label>
-            <BaseLabel>Duration</BaseLabel>
-            <TimePicker v-model.number="examSeconds" />
+  <div v-if="course" class="form">
+    <BasePanel class="form__panel">
+      <header class="form__header">
+        Create a new exam for {{ course.name }}
+      </header>
+      <FormError class="form__error" v-show="error">
+        {{ error }}
+      </FormError>
+      <div class="form__details">
+        <div class="form__detail">
+          <label for="name">
+            <BaseLabel>Name</BaseLabel>
           </label>
+          <BaseInput type="text" id="name" v-model="examName" />
         </div>
-        <div class="px-6">
+        <div class="form__detail">
+          <label for="duration">
+            <BaseLabel>Duration</BaseLabel>
+          </label>
+          <TimePicker id="duration" v-model.number="examSeconds" />
+        </div>
+        <div class="form__detail">
           <label for="attempts">
             <BaseLabel>Attempts</BaseLabel>
           </label>
@@ -30,7 +31,7 @@
             id="attempts"
           />
         </div>
-        <div class="px-6">
+        <div class="form__detail">
           <label for="week">
             <BaseLabel>Week</BaseLabel>
           </label>
@@ -41,52 +42,49 @@
             id="week"
           />
         </div>
-        <div class="px-6">
+        <div class="form__detail">
           <label for="startDate">
             <BaseLabel>Start Date</BaseLabel>
           </label>
           <DatePicker id="startDate" v-model="startDate" />
         </div>
-        <div class="pl-6">
+        <div class="form__detail">
           <label for="endDate">
             <BaseLabel>End Date</BaseLabel>
           </label>
           <DatePicker id="endDate" v-model="endDate" />
         </div>
       </div>
-      <div class="mt-4">
-        <div>
-          <ExamItemInput
-            v-model:question="examItem.question"
-            v-model:answer="examItem.answer"
-            v-model:question-type="examItem.questionType"
-            v-model:choices="examItem.choices"
-            v-for="(examItem, i) in examItems"
-            :count="i + 1"
-            :key="i"
-            @discard="removeExamItem(i)"
-            @add-choice="
-              examItem.choices.push(`Choice ${examItem.choices.length + 1}`)
-            "
-            @add-question="addExamItem(i + 1)"
-            class="flex mb-4"
-          />
-        </div>
-        <div class="flex justify-between">
-          <div>
-            <BaseButton @click="addExamItem()">Add Question</BaseButton>
-          </div>
-          <div class="ml-2">
-            <BaseButton @click="saveExam" prominent>Save Exam</BaseButton>
-          </div>
-        </div>
+      <div class="form__exam-items">
+        <ExamItemInput
+          v-model:question="examItem.question"
+          v-model:answer="examItem.answer"
+          v-model:question-type="examItem.questionType"
+          v-model:choices="examItem.choices"
+          v-for="(examItem, i) in examItems"
+          :count="i + 1"
+          :key="i"
+          @discard="removeExamItem(i)"
+          @add-choice="
+            examItem.choices.push(`Choice ${examItem.choices.length + 1}`)
+          "
+          @add-question="addExamItem(i + 1)"
+          class="flex mb-4"
+        />
       </div>
+      <footer class="form__footer">
+        <BaseButton @click="addExamItem()">Add Question</BaseButton>
+        <BaseButton @click="saveExam" :disabled="!valid" prominent>
+          Save Exam
+        </BaseButton>
+      </footer>
     </BasePanel>
   </div>
 </template>
 
 <script lang="ts">
 import BaseButton from '@/components/BaseButton.vue'
+import BaseInput from '@/components/BaseInput.vue'
 import BaseLabel from '@/components/BaseLabel.vue'
 import BasePanel from '@/components/BasePanel.vue'
 import DatePicker from '@/components/DatePicker.vue'
@@ -98,10 +96,12 @@ import { ALERT } from '@/store/action-types'
 import { ADD_EXAM } from '@/store/mutation-types'
 import { Course, ExamItem, NewExam, QuestionType } from '@/types'
 import { defineComponent } from 'vue'
+import dayjs from 'dayjs'
+import FormError from '@/components/FormError.vue'
 
 export default defineComponent({
   name: 'ExamCreationPage',
-  components: { BaseButton, BasePanel, BaseLabel, TimePicker, NumberInput, ExamItemInput, DatePicker },
+  components: { BaseButton, BasePanel, BaseLabel, TimePicker, NumberInput, ExamItemInput, DatePicker, BaseInput, FormError },
   props: {
     courseId: {
       type: String,
@@ -110,7 +110,7 @@ export default defineComponent({
   },
   data () {
     return {
-      examName: 'New Exam',
+      examName: '',
       examSeconds: 3600,
       maxAttempts: 3,
       week: 1,
@@ -128,6 +128,64 @@ export default defineComponent({
   computed: {
     course (): Course | undefined {
       return this.$store.getters.courseByID(this.courseId)
+    },
+    questionsInvalid (): string {
+      if (!this.examItems.length) {
+        return 'Please create an exam item.'
+      }
+
+      let error = ''
+      this.examItems.forEach((item, i) => {
+        if (error) return
+        if (!item.question) {
+          error = `Please enter a question for number ${i + 1}`
+          return
+        }
+        if (!item.answer.length || item.answer.includes('')) {
+          error = `Please enter an answer for number ${i + 1}`
+          return
+        }
+        if (item.questionType !== 'text') {
+          if (!item.choices.length) {
+            error = `Please create choices for number ${i + 1}`
+            return
+          }
+          if (item.choices.includes('')) {
+            error = `Please fill out all choices for number ${i + 1}`
+          }
+        }
+      })
+      return error
+    },
+    dateInvalid (): string {
+      if (!this.startDate || !this.endDate) {
+        return 'Please set the start and end dates for the exam.'
+      }
+      if (!dayjs(this.startDate).isBefore(this.endDate)) {
+        return 'The start date must be before the end date.'
+      }
+      if (!dayjs().isBefore(this.endDate)) {
+        return 'The end date must be set after today.'
+      }
+      return ''
+    },
+    error (): string {
+      if (!this.examName) {
+        return 'Please enter a name for the exam.'
+      }
+      if (this.examSeconds === 0) {
+        return 'Please set a valid duration.'
+      }
+      if (this.dateInvalid) {
+        return this.dateInvalid
+      }
+      if (this.questionsInvalid) {
+        return this.questionsInvalid
+      }
+      return ''
+    },
+    valid (): boolean {
+      return !this.error
     }
   },
   methods: {
@@ -172,3 +230,35 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="postcss" scoped>
+.form {
+  @apply p-4;
+}
+
+.form__panel {
+  @apply flex flex-col;
+}
+
+.form__error {
+  @apply mt-4 self-start;
+}
+
+.form__header {
+  @apply text-xl;
+}
+
+.form__details,
+.form__footer {
+  @apply flex justify-between;
+}
+
+.form__details {
+  @apply space-x-2;
+}
+
+.form__details,
+.form__exam-items {
+  @apply mt-4;
+}
+</style>
