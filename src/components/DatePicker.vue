63e0458c1d7945cd6b208e69297d1c90dcf3d150
Joshua Abad <yeyoan@gmail.com>
2021-02-18 21:40:29 +0800

    Style DatePicker button

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <BaseButton @click="isOpen = true">{{
-      formattedDate === "Invalid Date" ? "Choose Date" : formattedDate
-    }}</BaseButton>
+    <BaseButton class="date-picker-btn" @click="isOpen = true">
+      <svg
+        class="date-picker-btn__icon"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <div class="date-picker-btn__selected">
+        {{ dateSelected ? formattedDate : "Select Date" }}
+      </div>
+    </BaseButton>
     <teleport to="#modals">
       <transition name="modal-fade"
         ><div class="fixed z-30 inset-0 overflow-y-auto" v-show="isOpen">
@@ -60,8 +73,21 @@ export default defineComponent({
   },
   computed: {
     formattedDate (): string {
-      return dayjs(this.modelValue).format('MMMM D, YYYY')
+      return dayjs(this.modelValue).format('D MMM YYYY')
+    },
+    dateSelected (): boolean {
+      return this.formattedDate !== 'Invalid Date'
     }
   }
 })
 </script>
+
+<style lang="postcss" scoped>
+.date-picker-btn {
+  @apply flex items-center space-x-1 font-semibold text-base shadow bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 px-3 py-2;
+}
+
+.date-picker-btn__icon {
+  @apply w-4 h-4 fill-current;
+}
+</style>
