5227d09317c7cdf8a6a40d262ebaa863a84d6050
Joshua Abad <yeyoan@gmail.com>
2021-03-04 22:52:19 +0800

    Give Timeline panel fixed width so it's not janky

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="p-4 flex flex-col sm:flex-row items-start">
+  <div class="p-4 flex flex-col sm:flex-row">
     <AppPanel class="flex-grow space-y-8">
       <RecentCourses />
       <CourseList />
       <RecentActivity />
     </AppPanel>
-    <div class="ml-0 mt-4 w-full sm:w-1/3 sm:ml-4 sm:mt-0">
-      <Timeline />
+    <div class="ml-0 mt-4 w-full sm:w-72 sm:ml-4 sm:mt-0">
+      <Timeline class="w-72" />
     </div>
   </div>
 </template>
