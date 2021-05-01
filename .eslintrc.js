d01ba41f0a003aaf6e9975391ddb930f31397edb
Joshua Abad <yeyoan@gmail.com>
2021-05-01 22:29:05 +0800

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': 'vue-eslint-parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'parser': '@typescript-eslint/parser',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': [
+      'error',
+      'always'
+    ]
+  }
+}
