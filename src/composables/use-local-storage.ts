c4f649b1e7df9e5305ce1f1c23d0d037df2d6d8f
Joshua Abad <yeyoan@gmail.com>
2021-05-24 21:46:04 +0800

    Don't parse/stringify if value is already a string

--- src/composables/use-local-storage.ts
+++ src/composables/use-local-storage.ts
@@ -5,11 +5,20 @@ export default function useLocalStorage<T> (key: string, initialValue: T) {
 
   const storedData = localStorage.getItem(key)
   if (storedData) {
-    data.value = JSON.parse(storedData)
+    data.value = isString(data.value) ? storedData : JSON.parse(storedData)
+  }
+
+  function isString (x: unknown): x is string {
+    return typeof x === 'string'
   }
 
   watchEffect(() => {
-    localStorage.setItem(key, JSON.stringify(data.value))
+    localStorage.setItem(
+      key,
+      isString(data.value)
+        ? data.value
+        : JSON.stringify(data.value)
+    )
   })
 
   return data
