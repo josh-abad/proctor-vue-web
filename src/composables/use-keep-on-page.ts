bcebc16fbdc41331db5ac4cd128e92cb55b569e7
Joshua Abad <yeyoan@gmail.com>
2021-05-26 20:40:05 +0800

    A BOY IS A GUN*

---
+++ src/composables/use-keep-on-page.ts
@@ -0,0 +1,67 @@
+import { onMounted, onUnmounted, Ref } from 'vue'
+import useTimer from '@/composables/use-timer'
+import { onBeforeRouteLeave } from 'vue-router'
+
+interface KeepOnPageOptions {
+  preventLeave: Ref<boolean>;
+  onLeaveAttempt?: () => void;
+  onLeaveFocus?: () => void;
+  onLeaveTimeout?: () => void;
+  onUnload?: () => void;
+}
+
+export default function useKeepOnPage ({ preventLeave, onLeaveAttempt, onLeaveTimeout, onLeaveFocus, onUnload }: KeepOnPageOptions) {
+  const timer = useTimer(() => {
+    if (onLeaveTimeout !== undefined) {
+      onLeaveTimeout()
+    }
+    timer.start()
+  }, 5000)
+
+  const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+    e.preventDefault()
+    e.returnValue = ''
+  }
+
+  const handleBlur = () => {
+    if (onLeaveFocus !== undefined) {
+      onLeaveFocus()
+    }
+    timer.start()
+  }
+
+  const clearListeners = () => {
+    window.removeEventListener('beforeunload', handleBeforeUnload)
+    window.removeEventListener('blur', handleBlur)
+    window.removeEventListener('focus', timer.stop)
+
+    if (onUnload !== undefined) {
+      window.removeEventListener('unload', onUnload)
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('beforeunload', handleBeforeUnload)
+    window.addEventListener('blur', handleBlur)
+    window.addEventListener('focus', timer.stop)
+
+    if (onUnload !== undefined) {
+      window.addEventListener('unload', onUnload)
+    }
+  })
+
+  onUnmounted(() => {
+    clearListeners()
+  })
+
+  onBeforeRouteLeave((to, from, next) => {
+    if (preventLeave.value) {
+      if (onLeaveAttempt !== undefined) {
+        onLeaveAttempt()
+      }
+    } else {
+      clearListeners()
+      next()
+    }
+  })
+}
---
+++ src/composables/use-warning.ts
@@ -0,0 +1,51 @@
+import { computed, ref, watch } from 'vue'
+
+interface WarningOptions {
+  maximum: number,
+  onWarn?: () => void,
+  onExceed?: () => void
+}
+
+export default function useWarning ({ maximum, onWarn, onExceed }: WarningOptions) {
+  const warnings = ref(0)
+  const maxWarnings = ref(maximum)
+
+  const warningsExceeded = computed(() => {
+    return warnings.value === maxWarnings.value
+  })
+
+  const warningsLeft = computed(() => {
+    return maxWarnings.value - warnings.value
+  })
+
+  watch(warningsExceeded, isExceeded => {
+    if (onExceed !== undefined && isExceeded) {
+      onExceed()
+    }
+  })
+
+  const warn = (skipCallback = false) => {
+    if (!warningsExceeded.value) {
+      warnings.value++
+      // if (Notification.permission === 'granted') {
+      //   const notification = new Notification('Return to exam')
+      //   notification.addEventListener('click', () => {
+      //     window.focus()
+      //   })
+      // } else {
+      //   Notification.requestPermission()
+      // }
+      if (!skipCallback && onWarn !== undefined) {
+        onWarn()
+      }
+    }
+  }
+
+  return {
+    warnings,
+    warningsExceeded,
+    warningsLeft,
+    maxWarnings,
+    warn
+  }
+}
