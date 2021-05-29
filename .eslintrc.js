7abbb044603478f12516dbb1f188ab759faad2ce
Joshua Abad <yeyoan@gmail.com>
2021-05-29 22:32:56 +0800

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,25 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
-  'parser': 'vue-eslint-parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'parser': '@typescript-eslint/parser',
-    'sourceType': 'module'
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  'plugins': [
-    'vue',
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': [
-      'error',
-      'always'
-    ],
     '@typescript-eslint/explicit-module-boundary-types': 'off'
-  }
+  },
 }
