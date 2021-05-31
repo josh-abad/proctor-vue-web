3fbf97dc5d3c5d0a03a602d01be72cf32868ec97
Joshua Abad <yeyoan@gmail.com>
2021-05-31 20:42:32 +0800

    Add randomized question order

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,9 +3,26 @@ import dayjs from 'dayjs'
 import isBetween from 'dayjs/plugin/isBetween'
 dayjs.extend(isBetween)
 
-export function isExamLocked (exam: Exam): number {
+export function isExamLocked(exam: Exam): number {
   if (dayjs().isBetween(exam.startDate, exam.endDate, null, '[)')) {
     return 0
   }
   return dayjs().isAfter(exam.endDate) ? 1 : -1
+}
+
+/**
+ * Shuffle an array in-place.
+ * Uses Durstenfeld version of the Fisher-Yates shuffle algorithm.
+ *
+ * @param array this array to shuffle
+ * @returns the shuffled array
+ */
+export function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
 }
