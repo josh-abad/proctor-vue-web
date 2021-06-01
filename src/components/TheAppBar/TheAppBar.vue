b32f7e1ed589a2ebf8f687bd053056bd2c93d74a
Joshua Abad <yeyoan@gmail.com>
2021-06-01 13:04:39 +0800

    Make the app bar more compact

--- src/components/TheAppBar/TheAppBar.vue
+++ src/components/TheAppBar/TheAppBar.vue
@@ -1,29 +1,34 @@
 <template>
   <header
-    class="sticky top-0 z-20 flex items-center justify-between w-full h-20 py-4 bg-gray-100 bg-opacity-50 shadow-lg app-bar dark:bg-gray-800 dark:bg-opacity-75 app-bar__border"
+    class="sticky top-0 z-20 w-full bg-gray-200 animate h-14 dark:bg-gray-900"
+    :class="{
+      'shadow-xl': !isPageStart
+    }"
   >
-    <div class="flex items-center pl-6">
-      <button
-        class="block text-gray-500 focus:outline-none focus:ring-0"
-        @click="$emit('toggle')"
-      >
-        <MenuIcon class="w-5 h-5 fill-current" />
-      </button>
-      <router-link to="/">
-        <AppLogo class="ml-6 h-7" @click="$router.push('/')" />
-      </router-link>
-    </div>
-    <div class="flex items-center pr-6">
-      <div class="hidden mr-5 sm:block">
-        <Search />
+    <div class="flex items-center justify-between">
+      <div class="flex items-center pl-6">
+        <button
+          class="block text-gray-500 focus:outline-none focus:ring-0"
+          @click="$emit('toggle')"
+        >
+          <MenuIcon class="w-5 h-5 fill-current" />
+        </button>
+        <router-link to="/">
+          <AppLogo class="ml-6 h-7" @click="$router.push('/')" />
+        </router-link>
       </div>
-      <UserDropdown />
+      <div class="flex items-center pr-6">
+        <div class="hidden mr-5 sm:block">
+          <Search />
+        </div>
+        <UserDropdown />
+      </div>
     </div>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import UserDropdown from './components/UserDropdown.vue'
 import Search from './components/Search/Search.vue'
 import AppLogo from '../AppLogo.vue'
@@ -32,19 +37,31 @@ import { MenuIcon } from '@heroicons/vue/outline'
 export default defineComponent({
   name: 'TheAppBar',
   components: { UserDropdown, Search, AppLogo, MenuIcon },
-  emits: ['toggle']
+  emits: ['toggle'],
+  setup() {
+    const isPageStart = ref(true)
+
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 0) {
+        if (isPageStart.value) {
+          isPageStart.value = false
+        }
+      } else {
+        if (!isPageStart.value) {
+          isPageStart.value = true
+        }
+      }
+    })
+
+    return {
+      isPageStart
+    }
+  }
 })
 </script>
 
 <style lang="postcss" scoped>
-/* Fix for backdrop-filter not working in nested elements (UserDropdown and Search, in this case) */
-.app-bar::before {
-  content: "";
-  z-index: -1;
-  @apply absolute w-full h-full backdrop-filter backdrop-blur-lg;
-}
-
-.app-bar__border {
-  @apply border-b border-gray-800 border-opacity-10 dark:border-gray-100 dark:border-opacity-10;
+.animate {
+  @apply transition-shadow duration-300 ease-in-out;
 }
 </style>
