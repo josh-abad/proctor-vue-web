01689a79537ea682a67922d46dffc86afb3a35b9
Joshua Abad <yeyoan@gmail.com>
2021-06-04 12:29:07 +0800

    Refactor Navigation Drawer

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,36 @@
     <div>
       <TheAppBar v-if="$store.state.user" @toggle="handleToggle" />
       <div>
-        <TheSidebar v-if="$store.state.user" v-model:is-open="isOpen" />
+        <NavigationDrawer v-if="$store.state.user" :is-open="isOpen">
+          <NavigationItem href="/" @mobile-nav="isOpen = false">
+            <template #label> Home </template>
+            <template #icon>
+              <HomeIcon class="w-5 h-5" />
+            </template>
+          </NavigationItem>
+          <NavigationItem href="/courses" @mobile-nav="isOpen = false">
+            <template #label> Courses </template>
+            <template #icon>
+              <BookOpenIcon class="w-5 h-5" />
+            </template>
+          </NavigationItem>
+          <NavigationItem
+            href="/students"
+            v-if="$store.getters.permissions(['coordinator', 'admin'])"
+            @mobile-nav="isOpen = false"
+          >
+            <template #label> Students </template>
+            <template #icon>
+              <UsersIcon class="w-5 h-5" />
+            </template>
+          </NavigationItem>
+          <NavigationItem href="/calendar" @mobile-nav="isOpen = false">
+            <template #label> Calendar </template>
+            <template #icon>
+              <CalendarIcon class="w-5 h-5" />
+            </template>
+          </NavigationItem>
+        </NavigationDrawer>
         <div>
           <router-view
             class="duration-300 ease-in-out transform"
@@ -23,7 +52,7 @@
 import { defineComponent } from 'vue'
 import Snackbar from './components/Snackbar.vue'
 import TheAppBar from './components/TheAppBar/TheAppBar.vue'
-import TheSidebar from './components/TheSidebar/TheSidebar.vue'
+import NavigationDrawer from './components/NavigationDrawer.vue'
 import { SUBMIT_EXAM } from './store/action-types'
 import { SET_USER } from './store/mutation-types'
 import { AuthenticatedUser, Submission } from './types'
@@ -31,13 +60,25 @@ import authService from '@/services/auth'
 import cookie from '@/utils/cookie'
 import useLocalStorage from '@/composables/use-local-storage'
 import useTheme from '@/composables/use-theme'
+import NavigationItem from './components/NavigationItem.vue'
+import {
+  CalendarIcon,
+  UsersIcon,
+  BookOpenIcon,
+  HomeIcon
+} from '@heroicons/vue/solid'
 
 export default defineComponent({
   name: 'App',
   components: {
     TheAppBar,
-    TheSidebar,
-    Snackbar
+    NavigationDrawer,
+    Snackbar,
+    NavigationItem,
+    CalendarIcon,
+    UsersIcon,
+    BookOpenIcon,
+    HomeIcon
   },
   setup() {
     const isOpen = useLocalStorage('sidebarState', false)
