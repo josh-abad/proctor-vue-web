67d7e5a41094028af193272005a32033c0349b2f
Joshua Abad <yeyoan@gmail.com>
2021-06-04 17:45:00 +0800

    Keep router-view alive

--- src/views/courses/CoursePage/CoursePage.vue
+++ src/views/courses/CoursePage/CoursePage.vue
@@ -68,7 +68,9 @@
           <AppPanel class="overflow-hidden border-t-0 rounded-t-none">
             <router-view v-slot="{ Component, route }">
               <transition :name="route.meta.transition || 'fade'" mode="out-in">
-                <component :is="Component" />
+                <keep-alive>
+                  <component :is="Component" />
+                </keep-alive>
               </transition>
             </router-view>
           </AppPanel>
