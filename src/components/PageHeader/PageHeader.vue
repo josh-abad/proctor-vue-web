bfdb92b87101af69c97f2ed8ea3db24164eada8f
Joshua Abad <yeyoan@gmail.com>
2021-06-04 10:55:19 +0800

    Change background color

--- src/components/PageHeader/PageHeader.vue
+++ src/components/PageHeader/PageHeader.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="p-6 rounded-lg shadow-lg  bg-gradient-to-t from-green-600 via-green-500 to-green-400"
-  >
+  <div class="p-6 bg-green-500 rounded-lg shadow-lg">
     <div class="flex justify-between">
       <div class="text-2xl font-bold text-white sm:text-3xl">
         <slot name="label" />
