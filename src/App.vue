0ffdff1746f5ed6804648789af66a65b31e439c9
Joshua Abad <yeyoan@gmail.com>
2021-06-06 15:07:56 +0800

    Move global custom css to @layer base

--- src/App.vue
+++ src/App.vue
@@ -31,39 +31,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="postcss">
-::-webkit-scrollbar {
-  @apply w-3;
-}
-
-::-webkit-scrollbar-track {
-  @apply bg-gray-200 dark:bg-gray-800;
-}
-
-::-webkit-scrollbar-thumb {
-  @apply bg-gray-300 dark:bg-gray-700;
-}
-
-::-webkit-scrollbar-thumb:hover {
-  @apply bg-gray-400 dark:bg-gray-600;
-}
-
-.dropdown-fade-enter-active {
-  @apply transition duration-100 ease-out transform;
-}
-
-.dropdown-fade-leave-active {
-  @apply transition duration-75 ease-in transform;
-}
-
-.dropdown-fade-enter-from,
-.dropdown-fade-leave-to {
-  @apply scale-95 opacity-0;
-}
-
-.dropdown-fade-enter-to,
-.dropdown-fade-leave-from {
-  @apply scale-100 opacity-100;
-}
-</style>
