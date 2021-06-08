0d79bc801affc37385e9505cb35c6e7f8c43324d
Joshua Abad <yeyoan@gmail.com>
2021-06-08 16:58:50 +0800

    Improve modal animation

--- src/components/ui/AppModal.vue
+++ src/components/ui/AppModal.vue
@@ -1,18 +1,20 @@
 <template>
-  <transition name="modal-fade">
-    <div class="modal-backdrop" v-show="modelValue">
-      <div class="modal" v-click-outside="handleClickOutside">
-        <header class="modal-header">
-          <slot name="header">Dialog Header</slot>
-        </header>
-        <section class="modal-body">
-          <slot name="body">Dialog body text</slot>
-        </section>
-        <footer class="modal-footer">
-          <AppButton @click="closeModal">Cancel</AppButton>
-          <slot name="action" />
-        </footer>
-      </div>
+  <transition name="scrim-fade" v-show="modelValue">
+    <div class="modal-backdrop">
+      <transition name="modal-fade" v-show="modelValue">
+        <div class="modal" v-bind="$attrs" v-click-outside="handleClickOutside">
+          <header class="modal-header">
+            <slot name="header">Dialog Header</slot>
+          </header>
+          <section class="modal-body">
+            <slot name="body">Dialog body text</slot>
+          </section>
+          <footer class="modal-footer">
+            <AppButton @click="closeModal">Cancel</AppButton>
+            <slot name="action" />
+          </footer>
+        </div>
+      </transition>
     </div>
   </transition>
 </template>
@@ -32,6 +34,7 @@ export default defineComponent({
     }
   },
   emits: ['update:modelValue'],
+  inheritAttrs: false,
   setup(props, { emit }) {
     const closeModal = () => {
       emit('update:modelValue', false)
@@ -63,11 +66,11 @@ export default defineComponent({
 
 <style lang="postcss" scoped>
 .modal-backdrop {
-  @apply fixed z-30 top-0 bottom-0 left-0 bg-gray-900 bg-opacity-30 w-full h-full flex justify-center items-center;
+  @apply fixed z-30 top-0 bottom-0 left-0 bg-gray-900 bg-opacity-50 w-full h-full flex justify-center items-center transition-opacity;
 }
 
 .modal {
-  @apply overflow-x-auto flex flex-col bg-gray-100 dark:bg-gray-700 rounded-lg shadow-2xl;
+  @apply overflow-x-auto flex flex-col bg-gray-100 dark:bg-gray-700 rounded-lg shadow-2xl transition-all transform;
 }
 
 .modal-header,
@@ -88,20 +91,38 @@ export default defineComponent({
 }
 
 .modal-fade-enter-active {
-  @apply transition duration-300 ease-out transform;
+  @apply duration-300 ease-out;
 }
 
 .modal-fade-leave-active {
-  @apply transition duration-300 ease-in transform;
+  @apply duration-200 ease-in;
 }
 
 .modal-fade-enter-from,
 .modal-fade-leave-to {
-  @apply translate-y-4 opacity-0;
+  @apply translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95;
 }
 
 .modal-fade-enter-to,
 .modal-fade-leave-from {
-  @apply translate-y-0 opacity-100;
+  @apply translate-y-0 opacity-100 sm:scale-100;
+}
+
+.scrim-fade-enter-active {
+  @apply duration-300 ease-out;
+}
+
+.scrim-fade-leave-active {
+  @apply duration-200 ease-in;
+}
+
+.scrim-fade-enter-from,
+.scrim-fade-leave-to {
+  @apply opacity-0;
+}
+
+.scrim-fade-enter-to,
+.scrim-fade-leave-from {
+  @apply opacity-100;
 }
 </style>
