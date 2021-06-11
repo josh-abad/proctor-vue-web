de3cb1521c36346a863578d5345a1d6d031d1baa
Joshua Abad <yeyoan@gmail.com>
2021-06-11 20:39:53 +0800

    Fix AppAccordion justification

--- src/components/Subheading.vue
+++ src/components/Subheading.vue
@@ -1,12 +1,11 @@
 <template>
-  <header>
+  <header class="flex items-center justify-between pb-1">
     <slot></slot>
   </header>
 </template>
 
 <style lang="postcss" scoped>
 header {
-  @apply flex items-center justify-between pb-1;
   @apply border-b border-gray-300  dark:border-gray-700;
 }
 </style>
