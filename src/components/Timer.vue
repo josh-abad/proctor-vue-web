20ba704e95cc0949dc4aed499b33ad4a0c4919b1
Joshua Abad <yeyoan@gmail.com>
2021-06-14 17:42:07 +0800

    Boy, you got me helpless. Down for the count and I'm drownin' in 'em.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="flex items-center h-10 pr-3 overflow-hidden border-r border-gray-300 dark:border-gray-700">
-    <div class="text-lg font-semibold">
-      <span v-if="displayHours !== '00'"
-        >{{ displayHours }}<span class="font-thin">h</span></span
-      >
-      <span v-if="displayMinutes !== '00'"
-        >{{ displayMinutes }}<span class="font-thin">m</span></span
-      >
-      {{ displaySeconds }}<span class="font-thin">s</span>
+  <div class="flex items-center text-xl font-medium">
+    <ClockIcon class="w-6 h-6" />
+    <div class="ml-1.5">
+      {{ displayHours }}
+    </div>
+    :
+    <div>
+      {{ displayMinutes }}
+    </div>
+    :
+    <div>
+      {{ displaySeconds }}
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import { ClockIcon } from '@heroicons/vue/solid'
 import dayjs from 'dayjs'
 
 export default defineComponent({
   name: 'Timer',
+  components: { ClockIcon },
   props: {
     end: {
-      type: [Object, String] as PropType<Date | string>,
+      type: [Object, String, Number] as PropType<Date | string | number>,
       required: true
     }
   },
   emits: ['timer-ended'],
-  data () {
+  data() {
     return {
       displaySeconds: '0',
       displayMinutes: '0',
@@ -34,24 +39,24 @@ export default defineComponent({
   },
   computed: {
     seconds: (): number => 1000,
-    minutes (): number {
+    minutes(): number {
       return this.seconds * 60
     },
-    hours (): number {
+    hours(): number {
       return this.minutes * 60
     },
-    days (): number {
+    days(): number {
       return this.hours * 24
     }
   },
-  mounted () {
+  mounted() {
     this.countdown()
   },
   methods: {
-    format (n: number) {
+    format(n: number) {
       return n.toString().padStart(2, '0')
     },
-    countdown () {
+    countdown() {
       const timer = setInterval(() => {
         const distance = dayjs(this.end).diff(new Date())
 
