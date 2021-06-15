025c8d7ee022dc0a29aed13b804d70704df1ee47
Joshua Abad <yeyoan@gmail.com>
2021-06-15 11:11:14 +0800

    Fix display of duration

--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -282,11 +282,16 @@ export default defineComponent({
       return this.exam ? isExamLocked(this.exam) : 0
     },
     duration(): string {
-      return this.exam
-        ? dayjs
-            .duration({ hours: this.exam.duration / (60 * 60) })
-            .format('H [hour]')
-        : ''
+      const d = Math.floor(this.exam.duration / (3600 * 24))
+      const h = Math.floor((this.exam.duration % (3600 * 24)) / 3600)
+      const m = Math.floor((this.exam.duration % 3600) / 60)
+      const s = Math.floor(this.exam.duration % 60)
+
+      const dDisplay = d > 0 ? d + (d === 1 ? ' day, ' : ' days, ') : ''
+      const hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : ''
+      const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : ''
+      const sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : ''
+      return (dDisplay + hDisplay + mDisplay + sDisplay).replace(/,\s*$/, '')
     },
     date(): string {
       const format = (d?: Date) => {
