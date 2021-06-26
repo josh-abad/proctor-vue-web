87ec2b7e30b9cd2165929f7e14f26ace65c867ba
Joshua Abad <yeyoan@gmail.com>
2021-06-26 10:30:48 +0800

    Fix date picker position

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="date-picker" v-click-outside="closeModal">
-    <button class="date-picker-btn" @click="isOpen = !isOpen">
+  <div class="relative" v-click-outside="closeModal">
+    <button
+      class="
+        rounded-lg
+        focus:outline-none
+        flex
+        items-center
+        text-sm
+        shadow
+        bg-gray-100
+        dark:bg-gray-800
+        border border-gray-300
+        dark:border-gray-700
+        px-3
+        py-2
+      "
+      @click="isOpen = !isOpen"
+    >
       <div class="date-picker-btn__selected">
         {{ dateSelected ? formattedDate : 'Select Date' }}
       </div>
-      <CalendarIcon class="date-picker-btn__icon" />
+      <CalendarIcon class="w-4 h-4 ml-4" />
     </button>
     <transition name="dropdown-fade">
       <Calendar
         :model-value="modelValue"
         @update:modelValue="handleUpdate"
         @date-pick="closeModal"
-        class="date-picker__dropdown"
+        class="mt-2 origin-top-right z-30 w-80 absolute shadow-xl"
         v-show="isOpen"
         compact
       />
@@ -60,21 +76,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="postcss" scoped>
-.date-picker {
-  @apply relative;
-}
-
-.date-picker-btn {
-  @apply rounded-lg focus:outline-none flex items-center text-sm shadow bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 px-3 py-2;
-}
-
-.date-picker-btn__icon {
-  @apply w-4 h-4 ml-4;
-}
-
-.date-picker__dropdown {
-  @apply mt-2 origin-top-right z-30 w-80 absolute right-0 shadow-xl;
-}
-</style>
