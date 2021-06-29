d0c4fa69862d1d3761276366e3a06f2dbc7167af
Joshua Abad <yeyoan@gmail.com>
2021-06-29 10:53:34 +0800

    Make appearance more similar to EmptyState

--- src/components/ui/ErrorLoading.vue
+++ src/components/ui/ErrorLoading.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex flex-col items-center w-full py-4 text-gray-500">
-    <ExclamationIcon class="w-7 h-7" />
-    {{ message }}
+  <div class="flex flex-col items-center w-full py-10 text-gray-500">
+    <ExclamationIcon class="w-8 h-8" />
+    <span class="mt-4">{{ message }}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { ExclamationIcon } from '@heroicons/vue/solid'
+import { ExclamationIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
   name: 'ErrorLoading',
